c3a3f42ec0e8a8bf322c1fc4e4807870d8fe16eb
tamaralogar <tamara.logar@gmail.com>
2026-01-18 02:09:31 +0100

    urejanje signupa

--- SpletnaTrgovina/src/app/features/authentication/pages/signup/signup/signup.ts
+++ SpletnaTrgovina/src/app/features/authentication/pages/signup/signup/signup.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 import { User } from '../../../../../shared/classes/user';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import {AuthentificationService} from '../../../../../shared/services/authservice'
 
 @Component({
   selector: 'app-signup',
-  standalone: false,
+  standalone: false, 
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
@@ -13,19 +15,29 @@ export class Signup {
   public newUser: User;
   public confirmed: boolean = false;
 
-  constructor() {
+  constructor(private router : Router, private authService : AuthentificationService) {
     this.newUser = new User("", "", "", "");
    }
 
 public onSubmit(registrationForm: NgForm): void {
 
     if (this.confirmed && this.newUser.password === this.newUser.confirmPassword && registrationForm.valid) {
+      //Izpis ali je registracija uspešna ali ne
     let text = `Uspešno ste se registrirali: ${this.newUser.username} z e-poštnim naslovom: ${this.newUser.email} in geslom: ${this.newUser.password}`;
     alert(text);
     console.log(text);
     } else {
-        alert("Gesli se ne ujemata.");
-        console.log("Gesli se ne ujemata.");
+        alert("Gesli se ne ujemata. Niste registrirani");
+        console.log("Gesli se ne ujemata. Niste registrirani");
       }
+
+
+//Tukaj še dodaj!!!
+
   }
+
+
+
+
+
 }
