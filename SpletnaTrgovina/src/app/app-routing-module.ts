0b0ed41b44f08643bb8675ca3f8e5a123ffbf591
tamaralogar <tamara.logar@gmail.com>
2026-01-18 20:39:43 +0100

    routing finished

--- SpletnaTrgovina/src/app/app-routing-module.ts
+++ SpletnaTrgovina/src/app/app-routing-module.ts
@@ -9,14 +9,17 @@ import { ItemsDetails } from './features/items/pages/items-details/items-details
 import { Basket } from './features/items/pages/basket/basket/basket';
 import { Checkout } from './features/items/pages/checkout/checkout';
 
+//Guards
+import { AuthGuard } from './core/guards/auth.guard';
+
 const routes: Routes = [
   {    path: '', redirectTo: '/items', pathMatch: 'full' },
   {    path: 'login', component: Login   },
   {    path: 'signup', component: Signup   },
-  {    path: 'items', component: ItemsOverview   },
-  {    path: 'items/details', component: ItemsDetails   }, //tega zdej ni več
-  {    path: 'items/basket', component: Basket },
-  {    path: 'checkout', component: Checkout },
+  {    path: 'items', component: ItemsOverview, canActivate: [AuthGuard]   },
+  {    path: 'items/details', component: ItemsDetails, canActivate: [AuthGuard]   },
+  {    path: 'items/basket', component: Basket, canActivate: [AuthGuard] },
+  {    path: 'checkout', component: Checkout, canActivate: [AuthGuard] },
   {    path: '**', redirectTo: '/items'}
 ];
 
---
+++ SpletnaTrgovina/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthentificationService } from '../../shared/services/authservice';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthentificationService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
